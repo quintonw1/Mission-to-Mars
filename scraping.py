410986c0862ae9d300b02f89a5469ee7c3ba281d
Quinton Wandzura <quinton.wandzura2@ucalgary.ca>
2020-06-24 20:54:37 -0600

    uploading

--- scraping.py
+++ scraping.py
@@ -95,18 +95,6 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
 
-news_title, news_paragraph = mars_news(browser)
-
-
-# Run all scraping functions and store results in dictionary
-data = {
-      "news_title": news_title,
-      "news_paragraph": news_paragraph,
-      "featured_image": featured_image(browser),
-      "facts": mars_facts(),
-      "last_modified": dt.datetime.now()
-}
-
 if __name__ == "__main__":
     # If running as script, print scraped data
     print(scrape_all())
